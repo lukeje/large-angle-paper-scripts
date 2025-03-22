914d2234670fc7984bde385e0d885d60d32f8a13
Luke Edwards <ledwards@cbs.mpg.de>
2025-03-22 20:13:29 +0100

    Demonstration of dependence of parameter error on maximum flip angle for reviewer

---
+++ scripts/variableAngleError.jl
@@ -0,0 +1,30 @@
+using Pkg
+Pkg.activate(@__DIR__)
+
+# add MPMtools
+include(joinpath(@__DIR__, "external", "MPMtools.jl", "src", "MPMtools.jl"))
+using .MPMtools.MRIutils: dR1, dPD, optimalDFAparameters
+
+using Plots
+
+R1 = 0.82    # 1/s; in vivo WM 7T value in Rooney, et al. (2007)
+TR = 31.6e-3 # s;   TR used for in vivo experiments
+
+# optimal parameters and propagated error for estimating R1 without angle restriction
+trueopt = optimalDFAparameters(2*TR, R1; PDorR1="R1")
+σmin = dR1(R1,1.0,1.0,trueopt...)
+
+# optimal parameters and propagated errors with maximum flip angle restriction
+FAmax = range(5,rad2deg(maximum(trueopt[1:2])),length=100)
+opt = [optimalDFAparameters(2*TR, R1; PDorR1="R1", FAmax=FAmax) for FAmax in deg2rad.(FAmax)]
+σR1 = [dR1(R1,1.0,1.0,fa1,fa2,TR1,TR2) for (fa1,fa2,TR1,TR2) in opt]
+
+# plot relative increase in error due to flip angle restriction
+p = plot(FAmax,100*(σR1 .- σmin)/σmin, label="")
+ylims!(0,20)
+xlabel!("maximum allowed flip angle (°)")
+ylabel!("relative increase in error (%)")
+
+savefig(p, joinpath(dirname(@__DIR__),"figures","variableangleerror.png"))
+
+return p
