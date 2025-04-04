c9a01b108891c4169258c2663749dcd0a6c1a847
Luke Edwards <ledwards@cbs.mpg.de>
2025-04-04 14:19:00 +0200

    Add postmortem-like SNR

--- scripts/smallAngleSim.jl
+++ scripts/smallAngleSim.jl
@@ -21,79 +21,77 @@ TR     = (31.6, 70) .* 1e-3
 faPDw  = (5, 18)
 faT1w  = (27, 84)
 
-SNR = Inf
-
 Adifflim  = ((-0.5,2.5), ( -10, 50))
 R1difflim = ((-8,  2),   (-60, 15))
-for N in [1 2]
-    # use novel method
-    MRItypes.half_angle_tan(α) = 2tan(0.5α)
-    PDw = MRIutils.ernstd.(B1[N].*faPDw[N], TR[N], R1[N], PD=A)
-    T1w = MRIutils.ernstd.(B1[N].*faT1w[N], TR[N], R1[N], PD=A)
+for SNR in (18, 51, Inf) # in vivo, postmortem, no noise
+    for N in (1, 2)
+        # use novel method
+        MRItypes.half_angle_tan(α) = 2tan(0.5α)
+        PDw = MRIutils.ernstd.(B1[N].*faPDw[N], TR[N], R1[N], PD=A)
+        T1w = MRIutils.ernstd.(B1[N].*faT1w[N], TR[N], R1[N], PD=A)
 
-    A_est  = MRImaps.calculateA.(PDw, T1w)
-    R1_est = MRImaps.calculateR1.(PDw, T1w)
-    
-    # compute first order error propagation
-    σ = MRIutils.ernstd.(faPDw[N], TR[N], R1[N], PD=A).signal/SNR
-    dA = MRIutils.dPD.(PDw,T1w,σ,σ)
-    dR1 = MRIutils.dR1.(PDw,T1w,σ,σ)
+        A_est  = MRImaps.calculateA.(PDw, T1w)
+        R1_est = MRImaps.calculateR1.(PDw, T1w)
+        
+        # compute first order error propagation
+        σ = MRIutils.ernstd.(faPDw[N], TR[N], R1[N], PD=A).signal/SNR
+        dA = MRIutils.dPD.(PDw,T1w,σ,σ)
+        dR1 = MRIutils.dR1.(PDw,T1w,σ,σ)
 
-    # use small angle approximation
-    MRItypes.half_angle_tan(α) = α
-    PDwFA = MRIutils.ernstd.(B1[N].*faPDw[N], TR[N], R1[N], PD=A)
-    T1wFA = MRIutils.ernstd.(B1[N].*faT1w[N], TR[N], R1[N], PD=A)
+        # use small angle approximation
+        MRItypes.half_angle_tan(α) = α
+        PDwFA = MRIutils.ernstd.(B1[N].*faPDw[N], TR[N], R1[N], PD=A)
+        T1wFA = MRIutils.ernstd.(B1[N].*faT1w[N], TR[N], R1[N], PD=A)
 
-    A_FA_est  = MRImaps.calculateA.(PDwFA, T1wFA)
-    R1_FA_est = MRImaps.calculateR1.(PDwFA, T1wFA)
+        A_FA_est  = MRImaps.calculateA.(PDwFA, T1wFA)
+        R1_FA_est = MRImaps.calculateR1.(PDwFA, T1wFA)
 
-    # compute first order error propagation
-    dAFA = MRIutils.dPD.(PDwFA,T1wFA,σ,σ)
-    dR1FA = MRIutils.dR1.(PDwFA,T1wFA,σ,σ)
+        # compute first order error propagation
+        dAFA = MRIutils.dPD.(PDwFA,T1wFA,σ,σ)
+        dR1FA = MRIutils.dR1.(PDwFA,T1wFA,σ,σ)
 
-    local p1 = plot(100*B1[N], 100*(R1_est .- R1[N])./R1[N], label="", seriescolor=:blue, linewidth=2, ylims=R1difflim[N])
-    plot!(100*B1[N], 100*(R1_est .- R1[N] .- dR1)./R1[N], fillrange=100*(R1_est .- R1[N] .+ dR1)./R1[N], 
-                seriescolor=:blue, label=false, linewidth=0, seriestype=:steppre, alpha=0.25)
-    annotate!(100*(B1[N][end]), 100*(R1_est[end] - R1[N])/R1[N] - 0.01*-(ylims()...), ("new method", :bottom, :right, :black))
-    xticks!(50:25:150)
+        local p1 = plot(100*B1[N], 100*(R1_est .- R1[N])./R1[N], label="", seriescolor=:blue, linewidth=2, ylims=R1difflim[N])
+        plot!(100*B1[N], 100*(R1_est .- R1[N] .- dR1)./R1[N], fillrange=100*(R1_est .- R1[N] .+ dR1)./R1[N], 
+                    seriescolor=:blue, label=false, linewidth=0, seriestype=:steppre, alpha=0.25)
+        annotate!(100*(B1[N][end]), 100*(R1_est[end] - R1[N])/R1[N] - 0.01*-(ylims()...), ("new method", :bottom, :right, :black))
+        xticks!(50:25:150)
 
-    plot!(100*B1[N], 100*(R1_FA_est .- R1[N])./R1[N], label="", seriescolor=:red, linewidth=2, ylims=R1difflim[N])
-    plot!(100*B1[N], 100*(R1_FA_est .- R1[N] .- dR1FA)./R1[N], fillrange=100*(R1_FA_est .- R1[N] .+ dR1FA)./R1[N], 
-                seriescolor=:red, label=false, linewidth=0, seriestype=:steppost, alpha=0.25)
-    annotate!(100*(B1[N][end]), 100*(R1_FA_est[end] - R1[N])/R1[N] + 0.02*-(ylims()...), ("small angle method", :top, :right, :black))
-    xticks!(50:25:150)
+        plot!(100*B1[N], 100*(R1_FA_est .- R1[N])./R1[N], label="", seriescolor=:red, linewidth=2, ylims=R1difflim[N])
+        plot!(100*B1[N], 100*(R1_FA_est .- R1[N] .- dR1FA)./R1[N], fillrange=100*(R1_FA_est .- R1[N] .+ dR1FA)./R1[N], 
+                    seriescolor=:red, label=false, linewidth=0, seriestype=:steppost, alpha=0.25)
+        annotate!(100*(B1[N][end]), 100*(R1_FA_est[end] - R1[N])/R1[N] + 0.02*-(ylims()...), ("small angle method", :top, :right, :black))
+        xticks!(50:25:150)
 
-    # fudge factor for increase in axis needed to show errors
-    ylims!((ylims() .+ 400 .* [-1,1]/SNR)...)
+        # fudge factor for increase in axis needed to show errors
+        ylims!((ylims() .+ 400 .* [-1,1]/SNR)...)
 
-    ylabel!("relative R1 error (%)")
+        ylabel!("relative R1 error (%)")
 
-    local p2 = plot(100*B1[N], 100*(A_est .- A)./A, label="", seriescolor=:blue, linewidth=2, ylims=Adifflim[N])
-    plot!(100*B1[N], 100*(A_est .- A .- dA)./A, fillrange=100*(A_est .- A .+ dA)./A, 
-                seriescolor=:blue, label=false, linewidth=0, seriestype=:steppre, alpha=0.25)
-    annotate!(100*(B1[N][end]), 100*(A_est[end] - A)/A + 0.02*-(ylims()...), ("new method", :top, :right, :black))
-    xticks!(50:25:150)
+        local p2 = plot(100*B1[N], 100*(A_est .- A)./A, label="", seriescolor=:blue, linewidth=2, ylims=Adifflim[N])
+        plot!(100*B1[N], 100*(A_est .- A .- dA)./A, fillrange=100*(A_est .- A .+ dA)./A, 
+                    seriescolor=:blue, label=false, linewidth=0, seriestype=:steppre, alpha=0.25)
+        annotate!(100*(B1[N][end]), 100*(A_est[end] - A)/A + 0.02*-(ylims()...), ("new method", :top, :right, :black))
+        xticks!(50:25:150)
 
-    plot!(100*B1[N], 100*(A_FA_est .- A)./A, label="", seriescolor=:red, linewidth=2, ylims=Adifflim[N])
-    plot!(100*B1[N], 100*(A_FA_est .- A .- dAFA)./A, fillrange=100*(A_FA_est .- A .+ dAFA)./A, 
-                seriescolor=:red, label=false, linewidth=0, seriestype=:steppre, alpha=0.25)
-    annotate!(100*(B1[N][end]), 100*(A_FA_est[end] - A)/A - 0.01*-(ylims()...), ("small angle method", :bottom, :right, :black))
-    xticks!(50:25:150)
+        plot!(100*B1[N], 100*(A_FA_est .- A)./A, label="", seriescolor=:red, linewidth=2, ylims=Adifflim[N])
+        plot!(100*B1[N], 100*(A_FA_est .- A .- dAFA)./A, fillrange=100*(A_FA_est .- A .+ dAFA)./A, 
+                    seriescolor=:red, label=false, linewidth=0, seriestype=:steppre, alpha=0.25)
+        annotate!(100*(B1[N][end]), 100*(A_FA_est[end] - A)/A - 0.01*-(ylims()...), ("small angle method", :bottom, :right, :black))
+        xticks!(50:25:150)
 
-    # fudge factor for increase in axis needed to show errors
-    ylims!((ylims() .+ 200 .* [-1,1]/SNR)...)
+        # fudge factor for increase in axis needed to show errors
+        ylims!((ylims() .+ 200 .* [-1,1]/SNR)...)
 
-    xlabel!("B1 (p.u.)")
-    ylabel!(L"relative $A$ error (%)")
+        xlabel!("B1 (p.u.)")
+        ylabel!(L"relative $A$ error (%)")
 
-    if N==1
-        global p = plot(p1, p2, layout = (2,1), title=[names[N] " "])
-    else
-        global p = hcat(p,plot(p1, p2, layout = (2,1), title=[names[N] " "]))
+        if N==1
+            global p = plot(p1, p2, layout = (2,1), title=[names[N] " "])
+        else
+            global p = hcat(p,plot(p1, p2, layout = (2,1), title=[names[N] " "]))
+        end
     end
+
+    pl = plot(p[1],p[2], layout=(1,2), dpi=300, size=(1200,600), margin=5Plots.mm, plot_title=" ")
+    savefig(pl, joinpath(dirname(@__DIR__),"figures","simulation_SNR$(SNR).png"))
 end
-
-pl = plot(p[1],p[2], layout=(1,2), dpi=300, size=(1200,600), margin=5Plots.mm, plot_title=" ")
-savefig(pl, joinpath(dirname(@__DIR__),"figures","simulation_SNR$(SNR).png"))
-
-return pl
