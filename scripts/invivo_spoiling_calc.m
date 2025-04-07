512ce1e0d26cc93c95dfce9da2dc0c340f3d48d3
Luke Edwards <ledwards@cbs.mpg.de>
2025-04-07 13:09:39 +0200

    Include diffusion spoiling

--- scripts/invivo_spoiling_calc.m
+++ scripts/invivo_spoiling_calc.m
@@ -8,6 +8,15 @@ jobs = repmat(jobfile, 1, nrun);
 inputs = cell(1, nrun);
 for crun = 1:nrun
     inputs{1, crun} = {scriptdir}; % Imperfect Spoiling Calc.: Output directory - cfg_files
+
+    % gradient spoiling parameters
+    amp = 26; % mT/m
+    px = 400e-6; % m
+    spperpx = 6*pi;
+    gamma = 267.522; % rad/(ms mT)
+    dur = spperpx/(px*gamma*amp);
+    inputs{2, crun} = dur; % Imperfect Spoiling Calc.: Spoiler gradient duration (ms) - cfg_entry
+    inputs{3, crun} = amp; % Imperfect Spoiling Calc.: Spoiler gradient amplitude (mT/m) - cfg_entry
 end
 spm('defaults', 'FMRI');
 spm_jobman('run', jobs, inputs{:});
