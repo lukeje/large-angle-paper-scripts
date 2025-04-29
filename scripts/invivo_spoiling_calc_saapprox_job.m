41e1d1c995cd374b6d265ba1a76b9cba73439635
Luke Edwards <ledwards@cbs.mpg.de>
2025-04-29 10:32:11 +0200

    Compute imperf spoiling coeffs for small angle case, demonstrating they correct some of the error from using the small angle approximation

--- scripts/invivo_spoiling_calc_saapprox_job.m
+++ scripts/invivo_spoiling_calc_saapprox_job.m
@@ -4,7 +4,7 @@
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
 matlabbatch{1}.spm.tools.hmri.imperf_spoil.outdir = '<UNDEFINED>';
-matlabbatch{1}.spm.tools.hmri.imperf_spoil.prot_name = 'InVivo';
+matlabbatch{1}.spm.tools.hmri.imperf_spoil.prot_name = 'InVivo_smallangle';
 matlabbatch{1}.spm.tools.hmri.imperf_spoil.seq_params.FA_deg = [5 27];
 matlabbatch{1}.spm.tools.hmri.imperf_spoil.seq_params.TR_ms = [31.6 31.6];
 matlabbatch{1}.spm.tools.hmri.imperf_spoil.seq_params.Phi0_deg = 137;
@@ -14,4 +14,4 @@ matlabbatch{1}.spm.tools.hmri.imperf_spoil.seq_params.Gamp_mT_per_m = '<UNDEFINE
 matlabbatch{1}.spm.tools.hmri.imperf_spoil.tissue_params.T1range_ms = [1000 1080 1160 1240 1320 1400 1480 1560 1640 1720 1800 1880 1960 2040 2120 2200];
 matlabbatch{1}.spm.tools.hmri.imperf_spoil.tissue_params.T2range_ms = 45;
 matlabbatch{1}.spm.tools.hmri.imperf_spoil.tissue_params.D_um2_per_ms = 0.8;
-matlabbatch{1}.spm.tools.hmri.imperf_spoil.small_angle_approx = false;
+matlabbatch{1}.spm.tools.hmri.imperf_spoil.small_angle_approx = true;
--- scripts/postmortem_spoiling_calc.m
+++ scripts/postmortem_spoiling_calc.m
@@ -2,10 +2,11 @@ scriptdir = fileparts(mfilename('fullpath'));
 
 % List of open inputs
 % Imperfect Spoiling Calc.: Output directory - cfg_files
-nrun = 1; % enter the number of runs here
-jobfile = {fullfile(scriptdir,'postmortem_spoiling_calc_job.m')};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(1, nrun);
+jobs = {fullfile(scriptdir,'postmortem_spoiling_calc_job.m'),...
+    fullfile(scriptdir,'postmortem_spoiling_calc_saapprox_job.m')};
+
+nrun = length(jobfile); % enter the number of runs here
+inputs = cell(3, nrun);
 for crun = 1:nrun
     inputs{1, crun} = {scriptdir}; % Imperfect Spoiling Calc.: Output directory - cfg_files
 
